3eda306733513a0739722e338aa2b2ac2b77cfa7
Cameron <cameronpclay@gmail.com>
2024-05-27 19:05:00 -0600

    Tweaked benchmark and added data validation testing

--- MulithreadDataProcessorSTL/MulithreadDataProcessor/main.cpp
+++ MulithreadDataProcessorSTL/MulithreadDataProcessor/main.cpp
@@ -14,13 +14,13 @@
 #include <thread>
 #include <future>
 #include <execution>
-#include <barrier>
+#include <stdexcept>
 
 using std::chrono::high_resolution_clock;
 using std::chrono::duration;
 using namespace std::chrono_literals;
 
-const std::size_t NFUNC_CALLS = 3000000;
+const std::size_t NFUNC_CALLS = 4000000;
 const std::size_t NTRIALS = 100;
 const std::size_t THREAD_COUNT_MAX = std::thread::hardware_concurrency();
 
@@ -65,7 +65,7 @@ void GenerateData(std::vector<int>& data) {
 	std::generate(std::begin(data), std::end(data), [n = 0]() mutable { return n++; }); //reset vector to a list of indices to use as data
 }
 
-double BenchLinear(std::vector<int>& data, std::size_t threadIndex) {
+double BenchLinear(std::size_t trialIndex, std::vector<int>& data, std::size_t threadIndex) {
 	GenerateData(data);
 
 	const auto t1 = high_resolution_clock::now();
@@ -77,7 +77,7 @@ double BenchLinear(std::vector<int>& data, std::size_t threadIndex) {
 	return GetBenchResult(t1); //measure performance
 }
 
-double BenchMultithreaded(std::vector<int>& data, std::size_t threadIndex) {
+double BenchMultithreaded(std::size_t trialIndex, std::vector<int>& data, std::size_t threadIndex) {
 	GenerateData(data);
 
 	TaskPool taskPool;
@@ -109,7 +109,7 @@ double BenchMultithreaded(std::vector<int>& data, std::size_t threadIndex) {
 	return GetBenchResult(t1);
 }
 
-double BenchForEach(std::vector<int>& data, std::size_t threadIndex) {
+double BenchForEach(std::size_t trialIndex, std::vector<int>& data, std::size_t threadIndex) {
 	GenerateData(data);
 
 	const auto t1 = high_resolution_clock::now();
@@ -122,7 +122,7 @@ double BenchForEach(std::vector<int>& data, std::size_t threadIndex) {
 }
 
 //this is incredibly slow because it launches a new task NFUNC_CALLS times
-double BenchAsync(std::vector<int>& data, std::size_t threadIndex) {
+double BenchAsync(std::size_t trialIndex, std::vector<int>& data, std::size_t threadIndex) {
 	GenerateData(data);
 
 	const auto t1 = high_resolution_clock::now();
@@ -139,6 +139,12 @@ double BenchAsync(std::vector<int>& data, std::size_t threadIndex) {
 	return GetBenchResult(t1); //measure performance
 }
 
+void ValidateEqual(const std::vector<int>& data, const std::vector<int>& toCompare) {
+	if (!std::ranges::equal(data, toCompare)) {
+		throw std::logic_error("Alogorithm did not process correctly (data and toCompare not equal)...");
+	}
+}
+
 void HoldConsoleOpen() {
 	std::osyncstream(std::cout) << "\nPress any key to continue...";
 	std::string res;
@@ -149,30 +155,18 @@ void Benchmark() {
 	std::osyncstream(std::cout) << "Starting benchmark..." << "\n";
 
 	std::vector<int> data(NFUNC_CALLS);
+	std::vector<int> dataToCompare(NFUNC_CALLS);
 	std::vector<double> linearTime, multiTime, asyncTime, forEachTime;
 	linearTime.reserve(NFUNC_CALLS);
 	multiTime.reserve(NFUNC_CALLS);
 	asyncTime.reserve(NFUNC_CALLS);
 	forEachTime.reserve(NFUNC_CALLS);
-		
-	for (std::size_t threadIndex = 1u; threadIndex <= THREAD_COUNT_MAX; ++threadIndex) {
-		for (std::size_t trialIndex = 0u; trialIndex < NTRIALS; ++trialIndex) {
-			multiTime.push_back(BenchMultithreaded(data, threadIndex));
-		}
-
-		const double multiAvg = AvgBenchResults(multiTime);
-
-		multiTime.clear();
-
-		std::osyncstream(std::cout) << std::format("Avgs of {:d} trials of {:d} loops | multithreaded ({:d} threads) time = {:.3f}\n", NTRIALS, NFUNC_CALLS, threadIndex, multiAvg);
-	}
-
-	std::osyncstream(std::cout) << "--------------------------------------------------\n";
 
 	{
 		for (std::size_t trialIndex = 0u; trialIndex < NTRIALS; ++trialIndex) {
-			linearTime.push_back(BenchLinear(data, 0u));
-			forEachTime.push_back(BenchForEach(data, 0u));
+			linearTime.push_back(BenchLinear(trialIndex, dataToCompare, 0u));
+			forEachTime.push_back(BenchForEach(trialIndex, data, 0u));
+			ValidateEqual(data, dataToCompare); //validate all data was processed correctly (systems test)
 			//asyncTime.push_back(BenchAsync(data, 0u));
 		}
 
@@ -187,6 +181,22 @@ void Benchmark() {
 		std::osyncstream(std::cout) << std::format("Avgs of {:d} trials of {:d} loops | single threaded time = {:.3f} | std::for_each time = {:.3f} \n", NTRIALS, NFUNC_CALLS, linearAvg, forEachAvg);
 	}
 
+	std::osyncstream(std::cout) << "--------------------------------------------------\n";
+		
+	for (std::size_t threadIndex = 1u; threadIndex <= THREAD_COUNT_MAX; ++threadIndex) {
+		for (std::size_t trialIndex = 0u; trialIndex < NTRIALS; ++trialIndex) {
+			multiTime.push_back(BenchMultithreaded(trialIndex, data, threadIndex));
+			ValidateEqual(data, dataToCompare); //validate all data was processed correctly (systems test)
+		}
+
+		const double multiAvg = AvgBenchResults(multiTime);
+
+		multiTime.clear();
+
+		std::osyncstream(std::cout) << std::format("Avgs of {:d} trials of {:d} loops | multithreaded ({:d} threads) time = {:.3f}\n", NTRIALS, NFUNC_CALLS, threadIndex, multiAvg);
+	}
+
+	std::osyncstream(std::cout) << "--------------------------------------------------\n";
 	std::osyncstream(std::cout) << "Ending benchmark..." << "\n";
 }
 
